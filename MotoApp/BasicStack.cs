1df591ca11e90e952eb363b4b9bf81c1cd08bcf1
DESKTOP-596VCCI\Arkadiusz <arknie08576@wp.pl>
2023-11-01 17:17:50 +0100

    Add project files.

---
+++ MotoApp/BasicStack.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MotoApp
+{
+    internal class BasicStack<T>
+    {
+        private readonly T[] _items;
+        private int currentIndex = -1;
+        public BasicStack() => _items = new T[10];
+        public T Pop() => _items[currentIndex--];
+        public void Push(T item) => _items[++currentIndex] = item;
+        public int Count => currentIndex + 1;
+    }
+}
